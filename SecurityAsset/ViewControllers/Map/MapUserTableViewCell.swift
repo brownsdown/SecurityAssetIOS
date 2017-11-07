70d02db9d314ddeb2463d9e4806552dbf3e2b9d6
Michael <michaelmoldawski@hotmail.com>
2017-11-07 23:38:40 +0100

    Table view added to Map

---
+++ SecurityAsset/ViewControllers/Map/MapUserTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MapUserTableViewCell.swift
+//  SecurityAsset
+//
+//  Created by michael moldawski on 7/11/17.
+//  Copyright © 2017 michael moldawski. All rights reserved.
+//
+
+import UIKit
+
+class MapUserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
